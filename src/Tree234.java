1d9373f3f37a292261d4b8be4d4a7d029d9c564e
brancraig <bracraig@pdx.edu>
2018-08-13 16:20:17 -0700

    Providing javadoc documentation for the class. Trying to determine how much to provide for each node type--inline commenting might be best for each function but use copy-paste their purposes.
    Need to figure out which directory to store this in to not make it look like the project is written in html code.

--- src/Tree234.java
+++ src/Tree234.java
@@ -8,13 +8,24 @@ import java.util.*;
 /**
  * An implementation of a Sorted Set Collection, done as a 2-3-4 self-balancing tree. The public class itself acts as a
  * set of methods controlling a set private Node-class objects internally that generate the behavior expected for each
- * of the given methods. 
+ * of the given methods. Internal to the API-level function calls is a group of private node-class objects that
+ * implement the Node-interface; where each type of node handles different methods based on its type. For example, on an
+ * insertion of an object: Roots, Leaves, and Branches of each size (0, 2, 3, or 4 children) will handle that method
+ * dependent on the Node's type--this type-specific behavior is what allows the tree behave cohesively. Behavior for
+ * each method is for each node-type is described in that methods documentation.
+ *
  * @param <E> A parametrized object that can be stored in this collection. Must have a toCompare function implemented as
  *           this set creates a total ordering through comparison.
  */
 
 @SuppressWarnings("unchecked")
 public class Tree234<E> implements SortedSet<E> {
+    /**
+     * Main parametrizes an instance of the 2-3-4 tree as a string-based collection, and then inserts all substrings
+     * from the ubuntu-linux american-english dictionary--this is a remnant of an earlier usage of this tree that should
+     * be changed when more creative test examples are created.
+     * @param args command-line args not used.
+     */
     public static void main(String [] args){
         Tree234<String> tree234 = new Tree234<String>(String::compareTo);
         String filename = "/usr/share/dict/american-english"; //must be linux/ubuntu
@@ -67,6 +78,11 @@ public class Tree234<E> implements SortedSet<E> {
     }
     private Node root;
     private Comparator comparator;
+
+    /**
+     * The 2-3-4 Tree class constructor. Upon instantiation creates a new "Seed1" that is an empty root-node.
+     * @param comparator the comparator to be used in the absolute ordering of the set.
+     */
     private Tree234(Comparator<? super E> comparator){
         this.root = new Seed1();
         this.comparator = comparator;
@@ -75,6 +91,12 @@ public class Tree234<E> implements SortedSet<E> {
 
 
     //Begin SortedSet Required Functions://///////
+
+    /**
+     * Inserts an object of type E into the collection through a total ordering.
+     * @param toAdd the object to be added to the collection.
+     * @return true if the add added a new item to the set; false if that item was already present.
+     */
     @Override
     public boolean add(E toAdd) {
         Node oldRoot = this.root;
@@ -82,44 +104,80 @@ public class Tree234<E> implements SortedSet<E> {
         return oldRoot != this.root;
     }
 
+    /**
+     * Inserts a collection of objects of type E into this collection.
+     * @param c the collection of type E to be added to this collection.
+     * @return true if the addAll added at least one item not present in this collection.
+     */
     @Override
     public boolean addAll(Collection<? extends E> c) {
         return false;
     }
 
+    /**
+     * Empties the entire collection by re-instantiating the root-node as an empty "seed".
+     */
     @Override
     public void clear() {
         this.root = new Seed1();
     }
 
+    /**
+     * Gets the comparator that will be used in the total-ordering of the collection/set.
+     * @return the comparator to be used by the collection.
+     */
     @Override
     public Comparator<? super E> comparator() {
         return this.comparator;
     }
 
+    /**
+     * Determines whether or not an object is contained within the set.
+     * @param o the object to be found in the set.
+     * @return true if found, false if not found.
+     */
     @Override
     public boolean contains(Object o) {
         return false;
     }
 
+    /**
+     * Determines whether or not a collection (in its entirety) is contained within the set.
+     * @param c the collection to be found within this set.
+     * @return true if the entire collection is found, false if even one item is not found.
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
         return false;
     }
 
+    /**
+     * Retrieves the first element in the total ordering of objects contained within the set.
+     * @return the first-order objects if present, null if the set is empty.
+     */
     @Override
     public E first() {
         return null;
     }
 
+    /**
+     * Retrieves a sorted set (of type Tree234) containing all elements from the head to the specified element toElement
+     * @param toElement The element up to which all elements will be retrieved.
+     * @return
+     */
     @Override
     public SortedSet<E> headSet(E toElement) {
         return null;
     }
 
+    /**
+     * Determines whether the set is empty. Through the behavior of the internal classes; if the root-node is a SEED1
+     * and is empty, then the set is empty.
+     * @return true if empty, false is not-empty.
+     */
     @Override
     public boolean isEmpty() {
-        return root.getEnumeration() == NodeType.ROOTLEAF1 && root.contains(null);
+        return root.getEnumeration() == NodeType.SEED1 && root.contains(null);
     }
 
     @Override
@@ -127,11 +185,21 @@ public class Tree234<E> implements SortedSet<E> {
         return null;
     }
 
+
+    /**
+     * Retrieves the last element in the total ordering of objects contained within the set.
+     * @return the last-order objects if present, null if the set is empty.
+     */
     @Override
     public E last() {
         return null;
     }
 
+    /**
+     * Removes an object from the set.
+     * @param o the object to be removed.
+     * @return true if the object to be removed was present, false if it was not found.
+     */
     @Override
     public boolean remove(Object o) {
         Node newRoot = this.root.remove(o);
@@ -142,10 +210,16 @@ public class Tree234<E> implements SortedSet<E> {
         return true;
     }
 
+    /**
+     * Removes all object in a given collection from this set.
+     * @param c the collection to be removed.
+     * @return true if at least one object was removed, false if no objects were removed.
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
         return false;
     }
+
 
     @Override
     public boolean retainAll(Collection<?> c) {
@@ -157,16 +231,32 @@ public class Tree234<E> implements SortedSet<E> {
         return null;
     }
 
+    /**
+     * Retrieves a subset of this set containing all objects including and between to objects determined by their
+     * ordering.
+     * @param fromElement the first item in the ordering to be returned in the sub-collection.
+     * @param toElement the last item in the ordering to be returned in the sub-collection.
+     * @return
+     */
     @Override
-    public SortedSet<E> subSet(E fromElement, E toElement) {
-        return null;
-    }
+    public SortedSet<E> subSet(E fromElement, E toElement) { return null; }
 
+    /**
+     * Retrieves all elements from an element all the way to the last-ordered object in the set.
+     * @param fromElement the element from which all other elements to the tail will be included in the returned
+     *                    collection.
+     * @return the Sorted Set containing all elements from the param to the tail, or null if the parameter was not
+     * found within these set.
+     */
     @Override
-    public SortedSet<E> tailSet(E fromElement) {
-        return null;
-    }
+    public SortedSet<E> tailSet(E fromElement) { return null; }
 
+    /**
+     *
+     * @param array the array of type T that will contain the contents of this set with respect to their total-ordering.
+     * @param <T> The underlying parametrized type of the set to be returned in the array.
+     * @return an array of type T containing all elements of the set.
+     */
     @Override
     public <T> T[] toArray(T[] array) {
         int size = this.root.count();
@@ -184,53 +274,119 @@ public class Tree234<E> implements SortedSet<E> {
     }
 
     @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
+    public Object[] toArray() { return new Object[0]; }
 
     //END SortedSet Required Functions.////////////////
 
-
+    /**
+     * displays each element, in-order, by each elements given "toString" function.
+     */
     private void displayInOrder(){
         root.displayInOrder();
         System.out.println();
     }
 
-    private int count(){
-        return root.count();
-    }
+    /**
+     * counts the number of nodes (not objects) contained within the set.
+     * @return the number of nodes within the set.
+     */
+    private int count(){ return root.count();}
 
+    /**
+     * counts the number of elements within the set.
+     * @return the number of elements within the set.
+     */
     public int size(){ return root.size();}
 
+    /**
+     * determines the height/depth of the set.
+     * @return the height/depth of the set.
+     */
     private int height() { return root.height();}
 
+    /**
+     * checks to see if the tree is still balanced.
+     * @return true if the tree is balanced (all leaves are at the same depth), false if not balanced.
+     */
     private boolean verifyBalance(){
 
         return 0 != root.verifyBalance();
     }
 
-
+    /**
+     * The private enumerations that label each node as a certain type. Useful for certain functions when type-agnostic
+     * solutions are not determined.
+     */
     private enum NodeType {
-        ROOTLEAF1, ROOTLEAF2, ROOTLEAF3, ROOT1, ROOT2, ROOT3, LEAF1, LEAF2, LEAF3, BRANCH1, BRANCH2, BRANCH3, RETURN
+        SEED1, SEED2, SEED3, ROOT1, ROOT2, ROOT3, LEAF1, LEAF2, LEAF3, BRANCH1, BRANCH2, BRANCH3, RETURN
     }
 
+    /**
+     * An interface describing the functions each node must have to fulfill its role.
+     * @param <E> the type of the object contained as data members in each node.
+     */
     private interface Node<E>{
+        /**
+         * Adds an element E to a node.
+         * @param toAdd the element E to be added.
+         * @return the Node that contains E, or the node whose children contain E, or the original node if E is already
+         * present in the node or its children.
+         */
         Node add(E toAdd);
 
+        /**
+         * Finds an element toFind within the tree in logarithmic-time. Comparisons are made at each node to direct the
+         * path the algorithm takes.
+         * @param toFind the object to be found in the sorted set.
+         * @return true if found, false if not found.
+         */
         boolean contains(E toFind);
 
+        /**
+         * counts the number of NODES in the tree, not the number of elements of param E.
+         * @return the number of nodes in the tree.
+         */
         int count();
 
+        /**
+         * an in-order traversal that displays each object "toString"
+         */
         void displayInOrder();
 
+        /**
+         * Provided so that any node can give its type. The type itself is not stored as a data-member but as a function
+         * call to a definition.
+         * @return the enumeration of the node object.
+         */
         NodeType getEnumeration();
 
+        /**
+         * presuming that balancing of the tree is enforced, returns the height of the tree (by counting each node down
+         * the left side.)
+         * @return the height of the tree.
+         */
         int height();
 
+        /**
+         * Removal function. not yet described.
+         * @param toRemove The object to be removed.
+         * @return the new node no longer containing the thing to be removed, or removed in one of its children, or the
+         * original node (unchanged) because toRemove was not found in it or any of its descendants.
+         */
         Node remove(E toRemove);
 
+        /**
+         * counts the number of elements in a given node and calls the function on its children (by summing all nodes'
+         * counts).
+         * @return the count of elements in each node plus those of all its descendants.
+         */
         int size();
 
+        /**
+         * validates the balance of the tree by comparing the height of each child by each parent node; null signals
+         * failure.
+         * @return the distance of a node from the leaves of the tree.
+         */
         int verifyBalance();
 
 
@@ -238,8 +394,14 @@ public class Tree234<E> implements SortedSet<E> {
 
     }
 
+    /**
+     * This type of node is meant to be passed back through the return statement when the tree is splitting nodes on an
+     * insertion. This only happens when unresolved changes need to be communication towards the root of the tree,
+     * otherwise a reference to a non-ReturnNode (data-carrying node) is returned signaling that no more "rootward"
+     * adjustment is needed.
+     */
     private class ReturnNode implements Node<E> {
-        E pushUpData;
+        E pushUpData; //named because it is the data from the split node being "pushed up" the tree towards the root.
         Node leftChild;
         Node rightChild;
         ReturnNode(E pushUpData, Node leftChild, Node rightChild){
@@ -248,6 +410,11 @@ public class Tree234<E> implements SortedSet<E> {
             this.rightChild = rightChild;
         }
 
+        /**
+         * part of the node interface but not used in this nodetype. Rejects the addition of an element.
+         * @param toAdd the element E to be added, but it will always be rejected.
+         * @return always returns itself, unchanged.
+         */
         @Override
         public Node add(E toAdd) {
             return this;
@@ -349,7 +516,7 @@ public class Tree234<E> implements SortedSet<E> {
         public int verifyBalance(){return 1;}
 
         @Override
-        public NodeType getEnumeration() { return NodeType.ROOTLEAF1; }
+        public NodeType getEnumeration() { return NodeType.SEED1; }
 
 
     }
@@ -411,7 +578,7 @@ public class Tree234<E> implements SortedSet<E> {
 
         @Override
         public NodeType getEnumeration() {
-            return NodeType.ROOTLEAF2;
+            return NodeType.SEED2;
         }
 
     }
@@ -472,7 +639,7 @@ public class Tree234<E> implements SortedSet<E> {
         }
 
         @Override
-        public NodeType getEnumeration() { return NodeType.ROOTLEAF3; }
+        public NodeType getEnumeration() { return NodeType.SEED3; }
     }
 
     private class Root1 implements Node<E> {
